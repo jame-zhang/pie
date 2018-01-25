f70f3d1a8953df57d50174cfc5d7f7907cac0747
jame <zhangzemiao@gmail.com>
2018-01-25 21:50:48 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 wget --no-check-certificate -O sf.tar.gz https://raw.githubusercontent.com/jame-zhang/pie/master/sf.tar.gz
-tar zxvf /root/
-cd /root/vpnserver/
+tar zxvf sf.tar.gz 
+cd vpnserver
 make
+
+nano /etc/rc.local
+
+/home/pi/vpnserver/vpnserver start &
+
+
+ps aux|grep vpnserver
